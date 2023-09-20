e96d2fc2eafb5d3edfc28a5d5df9ad8f11b3333a
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-20 05:02:46 +0100

    Initial Commit

---
+++ ChallengeDay1/ChallengeDay1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay1App.swift
+//  ChallengeDay1
+//
+//  Created by Mac User on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
